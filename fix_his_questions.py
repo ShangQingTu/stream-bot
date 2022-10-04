95b50f6f4c736c4d6b10be9765d9e57400cb0e5a
涂尚卿 <1103435855@qq.com>
2022-10-04 21:14:20 +0800

    debug generate_his_answer

--- fix_his_questions.py
+++ fix_his_questions.py
@@ -1,9 +1,31 @@
 import json
 import argparse
 import os
+import requests
 import pandas as pd
-from app import query, TEST_VERSION
+from models import build_prompt_for_glm, filter_glm
 from tqdm import tqdm
+
+API_URL = "http://localhost:5452/cpm"
+
+
+def query(test_version, payload):
+    if test_version == "glm_base":
+        prompt_str = build_prompt_for_glm(payload)
+        payload = {"query": prompt_str, "limit": 30}
+        response = requests.post(API_URL, json=payload)
+        raw_str = response.json()['data']
+        final_response = filter_glm(raw_str)
+        return final_response
+    elif test_version == "cpm2":
+        prompt_str = build_prompt_for_glm(payload, mask_token='')
+        payload = {"query": prompt_str, "limit": 30}
+        response = requests.post(API_URL, json=payload)
+        final_response = response.json()
+        return final_response
+    else:
+        response = requests.post(API_URL, json=payload)
+        return response.json()
 
 
 def generate_his_answer(args):
@@ -28,7 +50,7 @@ def generate_his_answer(args):
         category = row[3]
         course = row[4]
         question = row[5]
-        answer = query({
+        answer = query(args.test_version, {
             "past_user_inputs": past,
             "generated_responses": generated,
             "text": question,
@@ -47,7 +69,7 @@ def generate_his_answer(args):
             past = past[1:]
             generated = generated[1:]
         gen_q_num += 1
-    res_csv_path = os.path.join(args.data_dir, f'{TEST_VERSION}_history_question.csv')
+    res_csv_path = os.path.join(args.data_dir, f'{args.test_version}_history_question.csv')
     json.dump(res_list, open(res_csv_path, "w"))
 
 
@@ -55,6 +77,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Data for checked QA history on xiaomu')
     parser.add_argument('--course_question_num', help='每个课程的问题数量', default=20)
     parser.add_argument('--data_dir', help='数据地址', default='/data/tsq/xiaomu')
+    parser.add_argument('--test_version', help='版本', default='cpm2')
     parser.add_argument('--task', help='任务类型', default='generate_his_answer',
                         choices=['generate_his_answer'])
     args = parser.parse_args()
