1083f8e9bc2b7a53c99c52cecb6b948a6b2553a2
涂尚卿 <1103435855@qq.com>
2022-11-08 20:54:24 +0800

    cot-10b

--- csv2html.py
+++ csv2html.py
@@ -2,35 +2,36 @@
 import pandas as pd
 import re
 
-# 将CSV文件转化为HTML文件
-# 标签头
+# 锟斤拷CSV锟侥硷拷转锟斤拷为HTML锟侥硷拷
+# 锟斤拷签头
 seg1 = '''<!DOCTYPE html><html lang="en"><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><head>
 <meta charset="GBK">
 <title>Title</title>
 </head>
 <body>
-<h2 align="center">xiaomu_130b</h2>\n
+<h2 align="center">xiaomu_cot10b_v2</h2>\n
 <table border="1" align="center" width="70%">\n
 <tr bgcolor="orange">\n'''
 
-# 将数据存入<table>标签中
-# 结尾标签
+# 锟斤拷锟斤拷锟捷达拷锟斤拷<table>锟斤拷签锟斤拷
+# 锟斤拷尾锟斤拷签
 seg2 = "</tr>\n"
 seg3 = "</table>\n</body>\n</html>"
 
 
-# 填充数据，参数为列表，全部居中显示，返回一个tr
+# 锟斤拷锟斤拷锟斤拷锟捷ｏ拷锟斤拷锟斤拷为锟叫憋拷锟斤拷全锟斤拷锟斤拷锟斤拷锟斤拷示锟斤拷锟斤拷锟斤拷一锟斤拷tr
 def fill_data(locls):
     seg = '<tr><td align="center">{}</td><td align="center">{}</td><td align="center">{}</td><td align="center">{}</td><td align="center">{}</td></tr>\n'.format(
         *locls)
     return seg
 
 
-# 获取csv文件，存入列表中
-df = pd.read_csv("202210222340_CoT_v2.3_glm_130B.csv")
+# 锟斤拷取csv锟侥硷拷锟斤拷锟斤拷锟斤拷锟叫憋拷锟斤拷
+df = pd.read_csv("cot10b.csv")
 print(df)
-ls = [["origin_id", "category", "course", "question", "answer"]]
+ls = [["origin_id", "ques_nickname", "course_id", "question", "answer", "time"]]
 for index, row in df.iterrows():
+    _id = row[0]
     origin_id = row[1]
     category = row[2]
     course = row[3]
@@ -40,19 +41,19 @@ for index, row in df.iterrows():
     try:
         answer = re.sub("\n", "<br>", answer)
     except TypeError:
-        answer = "这个老师没有教我，要不你问问别的～"
-    ls.append([origin_id, category, course, question, answer])
-# 写入HTML中
-with open("202210222340_CoT_v2.3_glm_130B.html", "w") as fw:
+        answer = "锟斤拷锟斤拷锟斤拷师没锟叫斤拷锟揭ｏ拷要锟斤拷锟斤拷锟斤拷锟绞憋拷锟侥★拷"
+    ls.append([_id, origin_id, category, course, question, answer])
+# 写锟斤拷HTML锟斤拷
+with open("cot10b.html", "w") as fw:
     fw.write(seg1)
-    # 添加表头：ls[0]是表头
+    # 锟斤拷锟接憋拷头锟斤拷ls[0]锟角憋拷头
     print(ls[0])
     fw.write(
         '<th width="25%">{}</th>\n<th width="25%">{}</th>\n<th width="25%">{}</th>\n<th width="25%">{}</th>\n<th width="25%">{}</th>\n'.format(
             *ls[0]))
     fw.write(seg2)
-    # 添加每一行
+    # 锟斤拷锟斤拷每一锟斤拷
     for i in range(1, len(ls)):
         fw.write(fill_data(ls[i]))
-    # 添加结尾标签
+    # 锟斤拷锟接斤拷尾锟斤拷签
     fw.write(seg3)
